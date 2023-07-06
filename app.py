4b248e4d63885578c3c9801d7b67803450632f4b
Bhovan-Dasari <20bq1a4216@vvit.net>
2023-07-06 22:30:56 +0530

    oop flask_restful implemented

--- app.py
+++ app.py
@@ -1,78 +1,79 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
 from pymongo import MongoClient
 from bson import ObjectId
 from pymongo.errors import DuplicateKeyError
+
 app = Flask(__name__)
+api = Api(app)
 
 # Connect to MongoDB
 # client = MongoClient('mongodb://localhost:27017/')
 # Connect to MongoDB
 client = MongoClient('mongodb://host.docker.internal:27017/')
-
 db = client['userdb']
 collection = db['users']
-@app.route("/",methods=['GET'])
-def hello():
-    return "hi",200
-
-# Endpoint to get all users
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = list(collection.find())
-    return jsonify(users), 200
 
 
-# Endpoint to get a specific user by ID
-@app.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = collection.find_one({'_id': user_id})
-    if user:
-        return jsonify(user), 200
-    else:
-        return 'User not found', 404
+class UserResource(Resource):
+    def get(self, user_id=None):
+        if user_id:
+            user = collection.find_one({'_id': user_id})
+            if user:
+                return user, 200
+            else:
+                return 'User not found', 404
+        else:
+            users = list(collection.find())
+            return users, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=int, required=True)
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('email', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        args = parser.parse_args()
+
+        user = {
+            '_id': args['id'],
+            'name': args['name'],
+            'email': args['email'],
+            'password': args['password']
+        }
+        try:
+            result = collection.insert_one(user)
+        except DuplicateKeyError:
+            return "User ID already exists. Please change the user ID.", 500
+        return 'User created successfully', 201
+
+    def put(self, user_id):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name', type=str, required=True)
+        parser.add_argument('email', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        args = parser.parse_args()
+
+        updated_user = {
+            'name': args['name'],
+            'email': args['email'],
+            'password': args['password']
+        }
+        result = collection.update_one({'_id': user_id}, {'$set': updated_user})
+        if result.modified_count > 0:
+            return 'User updated successfully', 200
+        else:
+            return 'User not found', 404
+
+    def delete(self, user_id):
+        result = collection.delete_one({'_id': user_id})
+        if result.deleted_count > 0:
+            return 'User deleted successfully', 200
+        else:
+            return 'User not found', 404
 
 
-# Endpoint to create a new user
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    user = {
-        '_id': int(data['id']),
-        'name': data['name'],
-        'email': data['email'],
-        'password': data['password']
-    }
-    try:
-        result = collection.insert_one(user)
-    except DuplicateKeyError:
-        return "User Id already exists, change the user id",500
-    return 'User created successfully', 201
-
-
-# Endpoint to update a user
-@app.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    data = request.get_json()
-    updated_user = {
-        'name': data['name'],
-        'email': data['email'],
-        'password': data['password']
-    }
-    result = collection.update_one({'_id': user_id}, {'$set': updated_user})
-    if result.modified_count > 0:
-        return 'User updated successfully', 200
-    else:
-        return 'User not found', 404
-
-
-# Endpoint to delete a user
-@app.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    result = collection.delete_one({'_id': user_id})
-    if result.deleted_count > 0:
-        return 'User deleted successfully', 200
-    else:
-        return 'User not found', 404
+api.add_resource(UserResource, '/users', '/users/<int:user_id>')
 
 
 if __name__ == '__main__':
